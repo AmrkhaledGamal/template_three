d2d1855efda0f9022577f754d5e9fa9b8afadb54
AmrkhaledGamal <amrkhaled45555@gmail.com>
2025-02-18 07:39:39 +0200

    edit skills

--- js/main.js
+++ js/main.js
@@ -30,3 +30,23 @@ menuAll.addEventListener("click", () => {
 landing.onclick = function () {
   megaMenu.classList.remove("mega-menu-open");
 };
+
+// skills
+let ourSkills = document.querySelector(".our-skills .skills");
+
+window.onscroll = function () {
+  let skillsOffsetTop = ourSkills.offsetTop;
+  // let skillsOffsetHeight = ourSkills.offsetHeight; = ourSkills.scrollHeight
+  let windowHeight = this.innerHeight;
+  // let windowScrollTop = this.pageYOffset;  = window.scrollY
+  if (
+    window.scrollY >
+    skillsOffsetTop + ourSkills.scrollHeight - windowHeight
+  ) {
+    let skillBar = document.querySelectorAll(".our-skills .content span");
+
+    skillBar.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
